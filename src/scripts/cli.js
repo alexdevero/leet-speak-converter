b771896feb8d89df381e8e139ddef207f74fd369
Alex Devero <deveroalex@gmail.com>
2017-10-18 16:17:13 +0200

    Move leet converter script into module

--- src/scripts/cli.js
+++ src/scripts/cli.js
@@ -4,60 +4,7 @@
 
 const readline = require('readline')
 
-const alphabetBasic = {
-  'a': '4',
-  'b': '8',
-  'e': '3',
-  'f': 'ph',
-  'g': '6', // or 9
-  'i': '1', // or |
-  'o': '0',
-  's': '5',
-  't': '7' // or +
-}
-
-const alphabetAdvanced = {
-  'c': '(', // or k or |< or /<
-  'd': '<|',
-  'h': '|-|',
-  'k': '|<', // or /<
-  'l': '|', // or 1
-  'm': '|\\/|',
-  'n': '|\\|',
-  'p': '|2',
-  'u': '|_|',
-  'v': '/', // or \/
-  'w': '//', // or \/\/
-  'x': '><',
-  'y': '\'/'
-}
-
-// Convert input into l33t
-const convertInput = (text, useAdvanced = 'n') => {
-  text = text || inputEl.value.toLowerCase()
-
-  for (let i = 0; i < text.length; i++) {
-    let alphabet
-    let letter = text[i].toLowerCase()
-
-    if (useAdvanced.toLowerCase() === 'y') {
-      // Use advanced l33t speak alphabet
-      alphabet = alphabetBasic[letter] ? alphabetBasic[letter] : alphabetAdvanced[letter]
-    } else {
-      // Use basic l33t speak alphabet
-      alphabet = alphabetBasic[letter]
-    }
-
-    if (alphabet) {
-      text = text.replace(text[i], alphabet)
-    }
-  }
-
-  // Show the result in console
-  console.log(text)
-
-  return text
-}
+const converter = require('./modules/leet-converter')
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -66,7 +13,7 @@ const rl = readline.createInterface({
 
 rl.question('What text would you like to convert to l33t speak? \n', (answerOne) => {
   rl.question('Do you want to use advanced l33t alphabet? (Y/N) ', (answerTwo) => {
-    convertInput(answerOne, answerTwo)
+    converter.convertInput(answerOne, answerTwo)
 
     rl.close()
   })
---
+++ src/scripts/modules/leet-converter.js
@@ -0,0 +1,56 @@
+const alphabetBasic = {
+  'a': '4',
+  'b': '8',
+  'e': '3',
+  'f': 'ph',
+  'g': '6', // or 9
+  'i': '1', // or |
+  'o': '0',
+  's': '5',
+  't': '7' // or +
+}
+
+const alphabetAdvanced = {
+  'c': '(', // or k or |< or /<
+  'd': '<|',
+  'h': '|-|',
+  'k': '|<', // or /<
+  'l': '|', // or 1
+  'm': '|\\/|',
+  'n': '|\\|',
+  'p': '|2',
+  'u': '|_|',
+  'v': '/', // or \/
+  'w': '//', // or \/\/
+  'x': '><',
+  'y': '\'/'
+}
+
+// Convert input into l33t
+const convertInput = (text, useAdvanced = 'n') => {
+  text = text
+
+  for (let i = 0; i < text.length; i++) {
+    let alphabet
+    let letter = text[i].toLowerCase()
+
+    if (useAdvanced.toLowerCase() === 'y') {
+      // Use advanced l33t speak alphabet
+      alphabet = alphabetBasic[letter] ? alphabetBasic[letter] : alphabetAdvanced[letter]
+    } else {
+      // Use basic l33t speak alphabet
+      alphabet = alphabetBasic[letter]
+    }
+
+    if (alphabet) {
+      text = text.replace(text[i], alphabet)
+    }
+  }
+
+  // Show the result in console
+  console.log(text)
+
+  return text
+}
+
+module.exports.convertInput = convertInput
