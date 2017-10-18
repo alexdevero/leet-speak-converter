321f9e622536126703af68ffa218c743bcfd79ca
Alex Devero <deveroalex@gmail.com>
2017-10-18 15:06:15 +0200

    Create node server

---
+++ src/scripts/server.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const http = require('http')
+const fs = require('fs')
+
+const port = 1337
+
+fs.readFile('./src/index.html', (err, html) => {
+  if (err) throw err
+
+  http.createServer((request, response) => {
+    response.writeHeader(200, { 'Content-Type': 'text/html' })
+    response.write(html)
+    response.end()
+  }).listen(port)
+
+  console.log(`Server is running on port ${port}.`)
+})
