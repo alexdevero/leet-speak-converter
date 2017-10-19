9382a929d9806a8662108469663cc7b1091df8f1
Alex Devero <deveroalex@gmail.com>
2017-10-19 10:58:39 +0200

    Add beta of web version of converter

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,48 +1,68 @@
 'use strict'
 
-(() => {
-  const alphabetBasic = {
-    'a': '4',
-    'b': '8',
-    'e': '3',
-    'f': 'ph',
-    'g': '6', // or 9
-    'i': '1', // or |
-    'o': '0',
-    's': '5',
-    't': '7' // or +
-  }
+const alphabetBasic = {
+  'a': '4',
+  'b': '8',
+  'e': '3',
+  'f': 'ph',
+  'g': '6', // or 9
+  'i': '1', // or |
+  'o': '0',
+  's': '5',
+  't': '7' // or +
+}
 
-  const alphabetAdvanced = {
-    'c': '(', // or k or |< or /<
-    'd': '<|',
-    'h': '|-|',
-    'k': '|<', // or /<
-    'l': '|', // or 1
-    'm': '|\\/|',
-    'n': '|\\|',
-    'p': '|2',
-    'u': '|_|',
-    'v': '/', // or \/
-    'w': '//', // or \/\/
-    'x': '><',
-    'y': '\'/'
-  }
+const alphabetAdvanced = {
+  'c': '(', // or k or |< or /<
+  'd': '<|',
+  'h': '|-|',
+  'k': '|<', // or /<
+  'l': '|', // or 1
+  'm': '|\\/|',
+  'n': '|\\|',
+  'p': '|2',
+  'u': '|_|',
+  'v': '/', // or \/
+  'w': '//', // or \/\/
+  'x': '><',
+  'y': '\'/'
+}
 
-  const inputEl = document.querySelector('.leetInput')
+// Convert input into l33t
+const convertInput = (text, useAdvanced = 'n') => {
+  for (let i = 0; i < text.length; i++) {
+    let alphabet
+    let letter = text[i].toLowerCase()
 
-  // Convert input into l33t
-  const convertInput = (text) => {
-    text = text || inputEl.value.toLowerCase()
-
-    for (var i = 0; i < text.length; i++) {
-      let alphabet = adv.checked ? alphabetBasic[text[i]] || alphabetAdvanced[text[i]] : alphabetBasic[text[i]]
-
-      if (alphabet) {
-        text = text.replace(text[i], alphabet)
-      }
+    if (useAdvanced.toLowerCase() === 'y') {
+      // Use advanced l33t speak alphabet
+      alphabet = alphabetBasic[letter] ? alphabetBasic[letter] : alphabetAdvanced[letter]
+    } else {
+      // Use basic l33t speak alphabet
+      alphabet = alphabetBasic[letter]
     }
 
-    return text
+    if (alphabet) {
+      text = text.replace(text[i], alphabet)
+    }
   }
-})()
+
+  return text
+}
+
+const buttonEl = document.querySelector('.leet-button')
+const checkboxEl = document.querySelector('.leet-checkbox')
+const resultEl = document.querySelector('.leet-result')
+const textareaEl = document.querySelector('.leet-textarea')
+
+buttonEl.addEventListener('click', () => {
+  let text = textareaEl.value
+  let useAdvanced = checkboxEl.checked ? 'y' : 'n'
+
+  console.log(useAdvanced)
+  console.log(checkboxEl.checked)
+
+  text = convertInput(text, useAdvanced)
+
+  resultEl.value = text
+})
