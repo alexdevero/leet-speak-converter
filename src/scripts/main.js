9a04f75c2dfd853ec6c57312664004125c77fee0
Alex Devero <deveroalex@gmail.com>
2017-10-21 09:37:12 +0200

    Create script for basic l33t -> text

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,6 +12,18 @@ const alphabetBasic = {
   't': '7' // or +
 }
 
+const alphabetBasicReversed = {
+  '4': 'a',
+  '8': 'b',
+  '3': 'e',
+  'ph': 'f',
+  '6': 'g', // or 9
+  '1': 'i', // or |
+  '0': 'o',
+  '5': 's',
+  '7': 't' // or +
+}
+
 const alphabetAdvanced = {
   'c': '(', // or k or |< or /<
   'd': '<|',
@@ -26,6 +38,22 @@ const alphabetAdvanced = {
   'w': '//', // or \/\/
   'x': '><',
   'y': '\'/'
+}
+
+const alphabetAdvancedReversed = {
+  '(': 'c', // or k or |< or /<
+  '<|': 'd',
+  '|-|': 'h',
+  '|<': 'k', // or /<
+  '|': 'l', // or 1
+  '|\\/|': 'm',
+  '|\\|': 'n',
+  '|2': 'p',
+  '|_|': 'u',
+  '/': 'v', // or \/
+  '//': 'w', // or \/\/
+  '><': 'x',
+  '\'/': 'y'
 }
 
 // Convert input into l33t
@@ -50,19 +78,48 @@ const convertInput = (text, useAdvanced = 'n') => {
   return text
 }
 
+const convertInputReverse = (text) => {
+  for (let i = 0; i < text.length; i++) {
+    let alphabet = ''/*alphabetBasicReversed[letter] ? alphabetBasicReversed[letter] : alphabetAdvancedReversed[letter]*/
+
+    let letter = text[i].toLowerCase()
+
+    if (alphabetAdvancedReversed[letter]) {
+      alphabet = alphabetAdvancedReversed[letter]
+    } else if (alphabetBasicReversed[letter]) {
+      alphabet = alphabetBasicReversed[letter]
+    } else {
+      alphabet = letter
+    }
+
+    console.log(alphabet)
+
+    if (alphabet) {
+      text = text.replace(text[i], alphabet)
+    }
+  }
+
+  return text
+}
+
 const buttonEl = document.querySelector('.leet-button')
 const checkboxEl = document.querySelector('.leet-checkbox')
+const checkboxReverseEl = document.querySelector('.leet-reverse')
 const resultEl = document.querySelector('.leet-result')
 const textareaEl = document.querySelector('.leet-textarea')
 
 buttonEl.addEventListener('click', () => {
   let text = textareaEl.value
   let useAdvanced = checkboxEl.checked ? 'y' : 'n'
+  let convertReversed = checkboxReverseEl.checked ? 'y' : 'n'
 
-  console.log(useAdvanced)
-  console.log(checkboxEl.checked)
-
-  text = convertInput(text, useAdvanced)
+  if (convertReversed.toLowerCase() === 'y') {
+    text = convertInputReverse(text)
+    console.log('reversed')
+  } else {
+    text = convertInput(text, useAdvanced)
+    console.log('regular')
+  }
 
   resultEl.value = text
 })
