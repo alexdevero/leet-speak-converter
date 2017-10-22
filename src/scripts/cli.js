f1c01a1ed3393d74518b08332fb4571f4a51a44f
Alex Devero <deveroalex@gmail.com>
2017-10-22 08:49:56 +0200

    Add option for reversed conversion

--- src/scripts/cli.js
+++ src/scripts/cli.js
@@ -2,17 +2,27 @@
 
 const readline = require('readline')
 
-const convertInput = require('./modules/leet-converter')
+const { convertInput, convertInputReverse } = require('./modules/leet-converter')
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 })
 
-rl.question('What text would you like to convert to l33t speak? \n', (answerOne) => {
-  rl.question('Do you want to use advanced l33t alphabet? (Y/N) ', (answerTwo) => {
-    convertInput(answerOne, answerTwo)
+rl.question('Do you want to convert text to l33t speak or l33t speak to text (latin)? (Y = text to l33t / N = l33t to text) \n', (answerZero) => {
+  if (answerZero.toLowerCase() === 'y') {
+    rl.question('What text would you like to convert to l33t speak? \n', (answerOne) => {
+      rl.question('Do you want to use advanced l33t alphabet? (Y/N) \n', (answerTwo) => {
+        convertInput(answerOne, answerTwo)
 
-    rl.close()
-  })
+        rl.close()
+      })
+    })
+  } else {
+    rl.question('What l33t speak text would you like to convert to text (latin)? \n', (answerThree) => {
+      convertInputReverse(answerThree)
+
+      rl.close()
+    })
+  }
 })
