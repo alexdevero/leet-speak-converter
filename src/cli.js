532f26281f2bd9a8ec093d4b94c862736442e7aa
Alex Devero <deveroalex@gmail.com>
2017-10-25 09:49:51 +0200

    Allow to convert another text

--- src/cli.js
+++ src/cli.js
@@ -9,20 +9,36 @@ const rl = readline.createInterface({
   output: process.stdout
 })
 
-rl.question('Do you want to convert text to l33t speak or l33t speak to text (latin)? (Y = text to l33t / N = l33t to text) \n', (answerZero) => {
-  if (answerZero.toLowerCase() === 'y') {
-    rl.question('What text would you like to convert to l33t speak? \n', (answerOne) => {
-      rl.question('Do you want to use advanced l33t alphabet? (Y/N) \n', (answerTwo) => {
-        convertInput(answerOne, answerTwo)
+const init = () => {
+  rl.question('Do you want to convert text to l33t speak or l33t speak to text (latin)? (Y = text to l33t / N = l33t to text) \n', (answerZero) => {
+    if (answerZero.toLowerCase() === 'y') {
+      rl.question('What text would you like to convert to l33t speak? \n', (answerOne) => {
+        rl.question('Do you want to use advanced l33t alphabet? (Y/N) \n', (answerTwo) => {
+          convertInput(answerOne, answerTwo)
 
-        rl.close()
+          rl.question('Do you want to convert another word? (Y/N) \n', (answerThree) => {
+            if (answerThree.toLowerCase() === 'y') {
+              init()
+            } else {
+              rl.close()
+            }
+          })
+        })
       })
-    })
-  } else {
-    rl.question('What l33t speak text would you like to convert to text (latin)? \n', (answerThree) => {
-      convertInputReverse(answerThree)
+    } else {
+      rl.question('What l33t speak text would you like to convert to text (latin)? \n', (answerThree) => {
+        convertInputReverse(answerThree)
 
-      rl.close()
-    })
-  }
-})
+        rl.question('Do you want to convert another word? (Y/N) \n', (answerFive) => {
+          if (answerFive.toLowerCase() === 'y') {
+            init()
+          } else {
+            rl.close()
+          }
+        })
+      })
+    }
+  })
+}
+
+init()
